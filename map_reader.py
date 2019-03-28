12efe23789ee7cdcd99e963f42539833b4445852
Thananan <minecraftskinman@gmail.com>
2019-03-28 13:07:43 +0700

    more convenient scale and add key press + release

--- map_reader.py
+++ map_reader.py
@@ -1,28 +1,27 @@
 import model
-UNIT_SIZE = 64
-HALF_SIZE = UNIT_SIZE/2
 def find_map(directory):
     return list(reversed(open(directory,'r').read().split('\n')))
-def read_map(directory,width,height):
+def read_map(directory,width,height,unit_size):
+    half_size = unit_size/2
     lst = find_map(directory)
     world = model.World(width,height)
     for string in range(len(lst)):
         line = lst[string]
-        y =  int(((string-1)*UNIT_SIZE) + HALF_SIZE)
+        y =  int(((string-1)*unit_size) + half_size)
         if line[0:5] == 'Enemy':
             component = line.split(',') # get [Enemy, str(x), ...
-            component = list(map(lambda x:int((int(x)*UNIT_SIZE) + HALF_SIZE), component[1:]))
+            component = list(map(lambda x:int((int(x)*unit_size) + half_size), component[1:]))
             world.create_enemy(component[0],component[1],component[2])
         if line[0:7] == 'S_Enemy':
             component = line.split(',') # get [S_Enemy, str(x), ...
-            component = list(map(lambda x:int((int(x)*UNIT_SIZE) + HALF_SIZE), component[1:]))
+            component = list(map(lambda x:int((int(x)*unit_size) + half_size), component[1:]))
             world.create_s_enemy(component[0],component[1])
 ##        elif line[0:3] == 'Map':
 ##            component = line.split(',') # get [Warp, str(x), ...
 ##            #generate warp - Warp(world,startx,starty,width,heigth,mapdirectory
         else:
             for symbol in range(len(line)):
-                x =  int((symbol*UNIT_SIZE) + HALF_SIZE)
+                x =  int((symbol*unit_size) + half_size)
                 if line[symbol] == 'M':
                     world.create_ground(x,y)
                 elif line[symbol] == 'X':
