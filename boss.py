2d8c37379891df022076164efe8785a874a47031
Thananan <minecraftskinman@gmail.com>
2019-05-03 16:17:10 +0700

    boss sprite

---
+++ boss.py
@@ -0,0 +1,12 @@
+class Boss:
+    def __init__(self,world):
+        self.body = 0
+        self.world = world
+        self.timer = 100
+    def random_attack_down(self):
+    def random_attack_left(self):
+    def random_attack_right(self):
+    def update(self):
+    def spawn(self):
+    def hurts(self,damage):
+    def die(self):
