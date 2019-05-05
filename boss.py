5da90bae6eb049238631ebb17d02a6fb6fa92bfd
Thananan <minecraftskinman@gmail.com>
2019-05-05 19:50:24 +0700

    down attack

--- boss.py
+++ boss.py
@@ -1,5 +1,6 @@
 import arcade
 import math
+import random
 MID_X = 408
 MID_Y = 300
 BOSS_BODY = ['images/boss/boss_body1.png',
@@ -26,6 +27,10 @@ BOSS_DOWN = ['images/boss/boss_down1.png',
              'images/boss/boss_down3.png',
              'images/boss/boss_down4.png',
              'images/boss/boss_down5.png']
+DOWN_ATTACK = ['images/boss/boss_down_attack4.png',
+             'images/boss/boss_down_attack3.png',
+             'images/boss/boss_down_attack2.png',
+             'images/boss/boss_down_attack1.png']
 class Boss:
     def __init__(self,world):
         self.body = 0
@@ -35,16 +40,22 @@ class Boss:
         self.cycle = 0
         self.top = arcade.Sprite('images/boss/boss_top.png')
         self.timer = 0
+        self.down_attack = 0
     def draw(self):
         self.body = arcade.Sprite(BOSS_BODY[self.cycle%5])
-        self.body.set_position(MID_X,MID_Y)
         self.left = arcade.Sprite(BOSS_LEFT[self.cycle%10])
-        self.left.set_position(MID_X,MID_Y)
         self.right = arcade.Sprite(BOSS_RIGHT[self.cycle%4])
-        self.right.set_position(MID_X,MID_Y)
         self.down = arcade.Sprite(BOSS_DOWN[self.cycle%5])
+        if self.down_attack > 0:
+            self.down = arcade.Sprite(DOWN_ATTACK[(self.down_attack%4)-1])
+            #draw attack
+            
+        self.body.set_position(MID_X,MID_Y)
+        self.left.set_position(MID_X,MID_Y)
+        self.right.set_position(MID_X,MID_Y)
         self.down.set_position(MID_X,MID_Y)
         self.top.set_position(MID_X + (math.sin(math.radians(self.top_angle)))*2+1,MID_Y + (math.cos(math.radians(self.top_angle))*10)+ 10)
+
         self.down.draw()
         self.body.draw()
         self.left.draw()
@@ -55,10 +66,19 @@ class Boss:
         if self.top_angle == 1440:
             self.top_angle = 0
         self.timer += 1
-        if self.timer == 15:
+        if self.timer == 5:
+            if self.down_attack > 0:
+                self.down_attack -= 1
+        if self.timer == 10:
             self.timer = 0
             self.cycle += 1
-##    def random_attack_down(self):
+            if self.cycle%4 == 0:
+                self.random_attack_down()
+            if self.down_attack > 0:
+                self.down_attack -= 1
+    def random_attack_down(self):
+        if random.randint(0,5) == 0:
+            self.down_attack = 5 
 ##    def random_attack_left(self):
 ##    def random_attack_right(self):
 ##    def update(self):
